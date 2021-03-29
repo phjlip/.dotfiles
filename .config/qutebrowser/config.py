9c95acc2bd76d205c26cbf1025415d5f241e71d1
Philjp <philipgottschall@gmail.com>
2021-03-29 12:36:40 +0200

    add xmodmap and qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,14 @@
+# type: ignore
+
+config.load_autoconfig(False)
+
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+c.content.notifications = False
+c.url.start_pages = ["google.com"]
+c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}",
+                        "ddg": "https://duckduckgo.com/?q={}"}
+c.auto_save.session = True
+
+config.bind("<space>sl" , "set-cmd-text -s :session-load", mode="normal")
+config.bind("<space>ss" , "set-cmd-text -s :session-save", mode="normal")
