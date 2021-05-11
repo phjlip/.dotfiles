d4069407900c4a675edb525d40cdbc4f8974b302
phjlip <philipgottschall@gmail.com>
2021-05-11 12:03:18 +0200

    reduce errors by commenting out

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,10 +1,11 @@
 # type: ignore
 
-config.load_autoconfig(False)
+# config.load_autoconfig(False)
 
-c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+# c.colors.webpage.darkmode.enabled = True
+# c.colors.webpage.darkmode.algorithm = "lightness-cielab"
 c.content.notifications = False
+c.backend = 'webengine'
 c.url.start_pages = ["google.com"]
 c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}",
                         "ddg": "https://duckduckgo.com/?q={}"}
