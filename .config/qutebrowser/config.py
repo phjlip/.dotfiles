4b78641ebf285764b484a181e331ed116265c8da
phjlip <philipgottschall@gmail.com>
2021-09-01 13:49:30 +0200

    standard search engine and font

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,10 +5,14 @@ config.load_autoconfig(False)
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = "lightness-cielab"
 c.content.notifications.enabled = False
-c.url.start_pages = ["google.com"]
-c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}",
-                        "ddg": "https://duckduckgo.com/?q={}"}
+c.url.start_pages = ["startpage.com"]
+c.url.searchengines = {
+    "gg": "https://www.google.com/search?q={}",
+    "ddg": "https://duckduckgo.com/?q={}",
+    "DEFAULT": "https://startpage.com/sp/search?query={}"
+}
 c.auto_save.session = True
+c.fonts.default_family = "Iosevka"
 
 config.bind("<space>sl" , "set-cmd-text -s :session-load", mode="normal")
 config.bind("<space>ss" , "set-cmd-text -s :session-save", mode="normal")
