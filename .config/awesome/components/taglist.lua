4ec2bf4b64c2737f8dd36140ff4942387c9e5f1a
phjlip <philipgottschall@gmail.com>
2021-11-03 20:39:37 +0100

    no client switching screens when pressing tag

--- .config/awesome/components/taglist.lua
+++ .config/awesome/components/taglist.lua
@@ -10,11 +10,11 @@ local taglist = {}
 
 local taglist_buttons = gears.table.join(
                     awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
+                    --[[ awful.button({ modkey }, 1, function(t)
                                               if client.focus then
                                                   client.focus:move_to_tag(t)
                                               end
-                                          end),
+                                          end), ]]
                     awful.button({ }, 3, awful.tag.viewtoggle),
                     awful.button({ modkey }, 3, function(t)
                                               if client.focus then
