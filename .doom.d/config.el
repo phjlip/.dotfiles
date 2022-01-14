9a44740b848ea1b01e608f8ff7eab22b5c7d602f
Philip <philipgottschall@gmail.com>
2022-01-14 18:03:52 +0100

    delete deprecated stuff

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -61,10 +61,6 @@
 
 
 (map! :map (outline-mode-map global-map evil-org-mode-map)
-      :niver "M-l" (lambda () (interactive) (evil-escape) (right-char))
-      :niver "M-h" 'evil-escape
-      :niver "M-j" (lambda () (interactive) (evil-escape) (evil-next-line 1))
-      :niver "M-k" (lambda () (interactive) (evil-escape) (evil-previous-line 1))
       :niver "C-s" 'evil-write
       :nv    "C-j" 'evil-forward-paragraph
       :nv    "C-k" 'evil-backward-paragraph)
@@ -79,21 +75,6 @@
       "M-l"    'org-agenda-date-later-minutes
       "M-h"    'org-agenda-date-earlier-minutes)
 
-(map! :leader :desc "Calendar" :g "o c" 'mycal)
-
-(defun mycal ()
-  (interactive)
-  (cfw:open-calendar-buffer
-   :view 'week
-   :contents-sources
-   (list
-    (cfw:org-create-file-source "Personal" "~/Dropbox/org/agenda/Personal.org" "Blue")  ; org-agenda source
-    (cfw:org-create-file-source "Work" "~/Dropbox/org/agenda/Personal.org" "Orange")  ; org-agenda source
-    (cfw:org-create-file-source "Anika" "~/Dropbox/org/agenda/Anika.org" "Gray"))))  ; org-agenda source
-
-(use-package! calfw
-  :config
-  (setq calendar-week-start-day 1))
 
 (use-package! auth-source
   :init
@@ -125,11 +106,6 @@
                   :calendar-id "work"
                   :files nil
                   :inbox "~/Dropbox/org/agenda/Work.org")))
-                 ;; (:url "https://moseskonto.tu-berlin.de/moses/cal/export/44d6e415b887f4b58c76f0f01e3b54a9"
-                 ;;  :calendar-id nil
-                 ;;  :files nil
-                 ;;  :inbox "~/Dropbox/org/agenda/Uni.org")))
-
   (add-hook 'org-agenda-mode-hook (lambda () (org-caldav-sync)))
   (add-hook 'org-capture-after-finalize-hook (lambda () (org-caldav-sync))))
 
@@ -137,70 +113,6 @@
 (use-package! plstore
   :config
   (setq plstore-cache-passphrase-for-symmetric-encryption t))
-
-
-(use-package! mu4e
-  :if (file-exists-p "~/.config/mu4e/mbsyncrc")
-  :load-path "/usr/share/emacs/site-lisp/mu4e"
-  :config
-  (setq mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
-        mu4e-change-filenames-when-moving t
-        mu4e-compose-format-flowed t
-        message-kill-buffer-on-exit t
-        mu4e-update-interval  (* 10 60)
-        message-send-mail-function 'smtpmail-send-it
-        mu4e-sent-folder "/pgottsch-up/Sent"
-        mu4e-drafts-folder "/pgottsch-up/Drafts"
-        mu4e-trash-folder "/pgottsch-up/Trash")
-
-
-  (setq mu4e-contexts
-        (list
-          ;; DEPRECATED Uni Potsdam
-          (make-mu4e-context
-          :name "UP"
-          :match-func
-            (lambda (msg)
-              (when msg
-                (string-prefix-p "/pgottsch-up" (mu4e-message-field msg :maildir))))
-          :vars '((user-mail-address . "pgottsch@uni-potsdam.de")
-                  (user-full-name    . "Philip Gottschall")
-                  (smtpmail-smtp-user  . "pgottsch@uni-potsdam.de")
-                  (smtpmail-smtp-server  . "smtp.uni-potsdam.de")
-                  (smtpmail-smtp-service . 465)
-                  (smtpmail-stream-type  . ssl)
-                  ;; (smtpmail-auth-credentials . (expand-file-name "~/.config/mu4e/authinfo.gpg"))
-                  (mu4e-maildir-shortcuts . (("/pgottsch-up/Inbox"      . ?i)
-                                              ("/pgottsch-up/Sent Mail" . ?s)
-                                              ("/pgottsch-up/Drafts"     . ?d)
-                                              ("/pgottsch-up/Trash"     . ?t)))
-                  (mu4e-sent-folder . "/pgottsch-up/Sent")
-                  (mu4e-drafts-folder . "/pgottsch-up/Drafts")
-                  (mu4e-refile-folder . "/pgottsch-up/Archive")
-                  (mu4e-trash-folder . "/pgottsch-up/Trash")))
-
-          ;; Personal
-          (make-mu4e-context
-          :name "Personal"
-          :match-func
-            (lambda (msg)
-              (when msg
-                (string-prefix-p "/philipgottschall-gmail" (mu4e-message-field msg :maildir))))
-          :vars '((user-mail-address . "philipgottschall@gmail.com")
-                  (user-full-name    . "Philip Gottschall")
-                  (smtpmail-smtp-user  . "philipgottschall@gmail.com")
-                  (smtpmail-smtp-server  . "smtp.gmail.com")
-                  (smtpmail-smtp-service . 465)
-                  (smtpmail-stream-type  . ssl)
-                  ;; (smtpmail-auth-credentials . (expand-file-name "~/.config/mu4e/authinfo.gpg"))
-                  (mu4e-maildir-shortcuts . (("/philipgottschall-gmail/Inbox"      . ?i)
-                                              ("/philipgottschall-gmail/Sent Mail" . ?s)
-                                              ("/philipgottschall-gmail/Drafts"     . ?d)
-                                              ("/philipgottschall-gmail/Trash"     . ?t)))
-                  (mu4e-sent-folder  . "/philipgottschall-gmail/Sent")
-                  (mu4e-drafts-folder  . "/philipgottschall-gmail/Drafts")
-                  (mu4e-refile-folder  . "/philipgottschall-gmail/All Mail")
-                  (mu4e-trash-folder  . "/philipgottschall-gmail/Trash"))))))
 
 
 (defun efs/org-font-setup ()
@@ -227,7 +139,7 @@
   ;; (set-face-attribute 'org-indent nil   :inherit '(org-hide fixed-pitch))
   (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :foreground "MediumPurple1" :bold t) ;:inherit '(shadow fixed-pitch)
   (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
   (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
   (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch))
@@ -261,8 +173,8 @@
           "~/Dropbox/org/agenda/Birthday.org"))
 
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "WIP(w)" "|" "DONE(d!)" "CANCEL(q)")
-          (sequence "IDEA(i)" "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "HOLD(h)" "|" "COMPLETED(c)" "CANCELED(k@)" "BACKLOOP(l)")))
+        '((sequence "WAIT(v)" "TODO(t)" "NEXT(n)" "WIP(w)" "|" "DONE(d!)" "CANCEL(c)")
+          (sequence "IDEA(i)" "PLAN(p)" "ACTIVE(a)" "HOLD(h)" "|" "FINISHED(f)" "CANCELED(q)" "BACKLOOP(l)")))
 
   (setq org-todo-keyword-faces
         '(("TODO" . "#ECBE7B") ("NEXT" . "#51afef")))
@@ -270,7 +182,6 @@
   (setq org-refile-targets
         '(("Archive.org" :maxlevel . 1)
           ("Tasks.org" :maxlevel . 1)))
-
 
   ;; Save Org buffers after refiling
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
@@ -319,29 +230,23 @@
       ((todo "IDEA"
              ((org-agenda-overriding-header "Ideas in Mind")
               (org-agenda-files org-agenda-files)))
-       (todo "REVIEW"
-             ((org-agenda-overriding-header "In Review")
-              (org-agenda-files org-agenda-files)))
        (todo "PLAN"
              ((org-agenda-overriding-header "In Planning")
               (org-agenda-todo-list-sublevels nil)
               (org-agenda-files org-agenda-files)))
-       (todo "BACKLOG"
-             ((org-agenda-overriding-header "Project Backlog")
-              (org-agenda-todo-list-sublevels nil)
-              (org-agenda-files org-agenda-files)))
-       (todo "READY"
-             ((org-agenda-overriding-header "Ready for Work")
+       (todo "HOLD"
+             ((org-agenda-overriding-header "Waiting for Parts")
               (org-agenda-files org-agenda-files)))
        (todo "ACTIVE"
              ((org-agenda-overriding-header "Active Projects")
               (org-agenda-files org-agenda-files)))
-       (todo "COMPLETED"
-             ((org-agenda-overriding-header "Completed Projects")
+       (todo "FINISHED"
+             ((org-agenda-overriding-header "Finished Projects")
               (org-agenda-files org-agenda-files)))
        (todo "CANCELED"
              ((org-agenda-overriding-header "Canceled Projects")
               (org-agenda-files org-agenda-files)))))))
+
 
   ;; Captures
   (setq org-capture-templates
@@ -363,20 +268,6 @@
       ("aw" "Work" entry (file "~/Dropbox/org/agenda/Work.org")
        "* %? %^{Event}\n %^T %i" :empty-lines 1))))
 
-
-;; (use-package! org-roam
-;;   :config
-;;   (setq org-roam-index-file "~/Dropbox/org/roam/brain.org"
-;;         org-roam-tag-sources '(prop all-directories))
-;;   (setq org-roam-capture-templates
-;;         '(("f" "New File" plain (function org-roam--capture-get-point)
-;;                :file-name "%(read-directory-name \"path: \" \"~/Dropbox/org/roam/\")/${slug}"
-;;                :head "#+title: ${title}\n#+created: %(format-time-string \"[%Y-%m-%d]\")\n#+roam_key: ${key}\n#+roam_tags: ${tags}\n\n%?"
-;;                :unnarrowed t)
-;;           ("n" "Note" plain (function org-roam--capture-get-point)
-;;                :file-name "%(read-directory-name \"path: \" \"~/Dropbox/org/roam/\")/${slug}"
-;;                :head "#+title: ${title}\n#+created: %(format-time-string \"[%Y-%m-%d]\")\n\n%?"
-;;                :unnarrowed t))))
 
 (use-package! org-roam
   :custom
